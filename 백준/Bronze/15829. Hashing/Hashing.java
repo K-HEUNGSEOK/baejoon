b6b3cee8d030ea6f91e5b2afaf85dcc6bf7db220
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-13 20:40:33 +0900

    [Bronze II] Title: Hashing, Time: 176 ms, Memory: 17736 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,36 +1,28 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       
+ 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        char [] arr = sc.next().toCharArray();
-        int result = getHashNumber(arr);
+        char[] arr = sc.next().toCharArray();
+        long result = getHashNumber(arr);
         System.out.println(result);
     }
 
-    private static int getHashNumber(char[] arr) {
-        int sum = 0 ;
-        for(int i = 0 ; i < arr.length; i ++){
-            sum += (int) (alphabetNumber(arr[i]) * Math.pow(31,i));
-        }
-        return sum % 1234567891;
-    }
+    private static long getHashNumber(char[] arr) {
+        long sum = 0;
+        long r = 31;
+        long mod = 1234567891;
+        long pow = 1; // 31^0
 
-    private static int alphabetNumber(char c){
-        char[] arr = {'a','b','c','d','e','f','g','h','i','j','k','l'
-        ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
-        for(int i = 0 ; i < arr.length; i ++){
-            if (arr[i] == c){
-                return  i + 1;
-            }
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i] - 'a' + 1; // 'a' → 1, 'b' → 2 ...
+            sum = (sum + num * pow) % mod;
+            pow = (pow * r) % mod;
         }
-        return 0;
+        return sum;
     }
 }
-
