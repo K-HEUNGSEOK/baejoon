9d02237db85d09eeff124159f3ae5d30bac9e3d6
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-24 19:48:58 +0900

    [Silver IV] Title: 수 찾기, Time: 1196 ms, Memory: 178156 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-   
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int [] a = new int[n];
@@ -17,15 +17,31 @@ public class Main {
         for(int i = 0 ; i < m ; i ++){
             b[i] = sc.nextInt();
         }
-        StringBuilder sb = new StringBuilder();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < n ; i ++){
-            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
-        }
-        for(int i = 0 ; i < m ; i ++){
-            sb.append(map.containsKey(b[i]) ? "1\n" : "0\n");
-        }
 
-        System.out.println(sb);
+        Arrays.sort(a);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < m ; i++){
+            if (binarySearch(a, b[i])) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+        }
+        System.out.print(sb);
     }
+
+    private static boolean binarySearch(int[] a, int target) {
+        int start = 0;
+        int end = a.length-1;
+        while (end >= start){
+            int mid = (start + end) /2 ;
+            if(a[mid] == target) return true;
+            else if(a[mid] > target){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return false;
     }
+}
