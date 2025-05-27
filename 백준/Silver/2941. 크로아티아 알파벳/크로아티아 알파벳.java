acbd65bafd6ff705838d0a47a29065efe2cf8e77
K-HEUNGSEOK <148242087+K-HEUNGSEOK@users.noreply.github.com>
2025-05-27 21:16:51 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 164 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -10,42 +10,18 @@ import java.util.*;
 public class Main {
     static String[] target = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
     public static void main(String[] args) throws IOException {
-   
+
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         print(str);
     }
     static void print(String str){
-        int cnt = 0;
-
-        for(int i = 0 ; i < str.length(); i ++){
-            boolean check = false;
-            if (i + 2 < str.length()){
-                String sub1 = str.substring(i , i + 3);
-                if (sub1.equals("dz=")){
-                    i+=2;
-                    cnt ++;
-                    continue;
-                }
+        for (String s : target) {
+            if (str.contains(s)){
+                str = str.replace(s, "*");
             }
-
-            if (i + 1 < str.length()){
-                String sub2 = str.substring(i,i+2);
-                for (String s : target) {
-                    if (sub2.equals(s)){
-                        cnt ++;
-                        i+=1;
-                        check = true;
-                        break;
-                    }
-                }
-            }
-            if (check) continue;
-
-            cnt ++;
         }
-        System.out.println(cnt);
-
+        System.out.println(str.length());
     }
 
 }
